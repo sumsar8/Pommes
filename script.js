ea7c79ccb582f0c02b8968daa372eaca0cc4b793
sumsar8 <60388964+sumsar8@users.noreply.github.com>
2021-06-20 18:56:24 +0200

    uiogy

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const downbutton = document.getElementById("down");
 function downup(){
-    downbutton.classList.toggle("")
+    document.getElementById("downboxid").classList.add("downboxanimate");   
+    setTimeout(() => {
+        document.getElementById("downboxid").innerHTML = "Upp";
+    }, 2000);
+    setTimeout(() => {
+        document.getElementById("downboxid").classList.remove("downboxanimate");
+    }, 5000);
 }
